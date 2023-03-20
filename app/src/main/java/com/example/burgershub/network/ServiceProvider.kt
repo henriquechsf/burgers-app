6c36083e8e07a22a8f530fc6592c50a9a708d5af
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-20 17:37:15 -0300

    config retrofit

--- app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
+++ app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
@@ -1,5 +1,24 @@
 package com.example.burgershub.network
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 class ServiceProvider {
 
+    private val baseUrl = ""
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build();
+
+    fun <T> createService(apiClass: Class<T>): T = retrofit.create(apiClass)
 }
