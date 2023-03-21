f1a96dce7cfa8ae2a1810db8f75459090b626cb6
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-03-21 08:45:26 -0300

    list burgers on fragment

--- app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
+++ app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
@@ -1,6 +1,8 @@
 package com.example.burgershub.network
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -12,6 +14,22 @@ class ServiceProvider {
     private val okHttpClient = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .addInterceptor(Interceptor { chain ->
+            chain.run {
+                proceed(
+                    request()
+                        .newBuilder()
+                        .addHeader(
+                            name = "X-RapidAPI-Key",
+                            value = "fd0f761f82mshe15b14a02cab83dp1c89c4jsn8089a23af69f"
+                        )
+                        .build()
+                )
+            }
+        })
         .build()
 
     private val retrofit = Retrofit.Builder()
--- app/src/main/java/com/example/burgershub/presenter/burgers/BurgersFragment.kt
+++ app/src/main/java/com/example/burgershub/presenter/burgers/BurgersFragment.kt
@@ -1,12 +1,17 @@
 package com.example.burgershub.presenter.burgers
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.burgershub.R
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.burgershub.databinding.FragmentBurgersBinding
+import com.example.burgershub.domain.model.Burger
+import com.example.burgershub.util.StateView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +20,8 @@ class BurgersFragment : Fragment() {
     private var _binding: FragmentBurgersBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: BurgerViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +29,42 @@ class BurgersFragment : Fragment() {
     ): View {
         _binding = FragmentBurgersBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getBurgers()
+    }
+
+    private fun getBurgers() {
+        viewModel.getBurgers().observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
+                is StateView.Loading -> {
+
+                }
+                is StateView.Success -> {
+                    binding.progressBar.isVisible = false
+
+                    val burgers = stateView.data ?: emptyList()
+                    initRecycler(burgers)
+                }
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun initRecycler(burgers: List<Burger>) {
+        with(binding.rvBurgers) {
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+            adapter = BurgersAdapter(burgers) { burgerId ->
+
+            }
+        }
     }
 
     override fun onDestroyView() {
